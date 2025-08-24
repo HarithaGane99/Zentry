ee649911f046026a9fab2419ba952b334e0363d4
Haritha Ganegoda <haritha.99ganegoda@gmail.com>
2025-08-24 18:20:55 +0530

    navbar + basic pages

---
+++ frontend/src/Components/AddUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddUser = () => {
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default AddUser
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../Styles/navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h2>Zentry</h2>
+      <ul className='home-ul'>
+        <li className='home ll'><Link to="/">Home</Link></li>
+        <li className='about ll'><Link to="/adduser">Add User</Link></li>
+        <li className='contact ll'><Link to="/userdetails">User Details</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
